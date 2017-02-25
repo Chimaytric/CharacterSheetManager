c2bd9a1134bb55136b908fa6b69b65fb9ce61e08
Charles MILLEQUANT <charles@cogitstudio.com>
2017-02-25 16:52:10 +0100

    Games and players
    Game and Player display
    Create/delete game
    Create/delete player

--- games/games.component.js
+++ games/games.component.js
@@ -1,8 +1,47 @@
-function GamesController(){
+function GamesController(gamesFactory){
     console.log('Games controller');
+
+    var vm = this;
+
+    vm.templates = [];
+    gamesFactory.getTemplates().then(function(templates){
+        vm.templates = templates;
+    });
+
+    vm.getGames = function(){
+        gamesFactory.getGames().then(function(games){
+            vm.games = games;
+            vm.games.forEach(function(game){
+                gamesFactory.getTemplateById(game.template).then(function(template){
+                    game.template = template;
+                });
+            });
+        });
+    }
+    vm.getGames();
+
+
+    vm.newGame = {};
+    vm.createGame = function(){
+        vm.newGame.template = vm.newGame.template.id;
+        gamesFactory.addGame(vm.newGame).then(function(response){
+            console.log(response);
+            vm.getGames();
+            vm.newGame = {};
+        });
+    }
+
+    vm.deleteGame = function(gameId){
+        if(confirm('Are you sure you want to delete this game ?')){
+            gamesFactory.deleteGame(gameId).then(function(response){
+                console.log(response);
+                vm.getGames();
+            });
+        }
+    }
 }
 
-GamesController.$inject = [];
+GamesController.$inject = ['gamesFactory'];
 
 angular.module('characterSheetmanager.gamesComponent', []).component('gamesComponent', {
     templateUrl: 'games/games.component.html',
@@ -12,4 +51,66 @@ angular.module('characterSheetmanager.gamesComponent', []).component('gamesCompo
         parent: '^rootComponent'
     },
     bindings: {}
+}).factory('gamesFactory', function($http){
+    return {
+        getGames: function(){
+            var url = "http://localhost:3000/games";
+            return $http({
+                method: 'GET',
+                url: url
+            }).then(function(response){
+                return response.data;
+            }).catch(function(response){
+                console.log("[Error] [getGames] . "+response.status+" : "+response.statusText);
+            });
+        },
+        addGame: function(game){
+            var url = "http://localhost:3000/games";
+            return $http({
+                method: 'POST',
+                url: url,
+                headers : {
+                    'Content-Type': 'application/json'
+                },
+                data: game
+            }).then(function(response){
+                return response;
+            }).catch(function(response){
+                console.log("[Error] [addgame] . "+response.status+" : "+response.statusText);
+            });
+        },
+        deleteGame: function(gameId){
+            var url = "http://localhost:3000/games/"+gameId;
+            return $http({
+                method: 'DELETE',
+                url: url
+            }).then(function(response){
+                return response.data;
+            }).catch(function(response){
+                console.log("[Error] [deleteGame] . "+response.status+" : "+response.statusText);
+            });
+        },
+        getTemplateById: function(templateId){
+            var url = "http://localhost:3000/templates/"+templateId;
+            return $http({
+                method: 'GET',
+                url: url
+            }).then(function(response){
+                return response.data;
+            }).catch(function(response){
+                console.log("[Error] [getTemplateById] . "+response.status+" : "+response.statusText);
+            });
+        },
+        getTemplates: function(){
+            var url = "http://localhost:3000/templates";
+            return $http({
+                method: 'GET',
+                url: url
+            }).then(function(response){
+                return response.data;
+            }).catch(function(response){
+                console.log("[Error] [getTemplates] . "+response.status+" : "+response.statusText);
+            });
+        }
+    }
 });
--- players/players.component.js
+++ players/players.component.js
@@ -1,8 +1,48 @@
-function PlayersController(){
+function PlayersController(playersFactory){
     console.log('Players controller');
+
+    var vm = this;
+
+    vm.newPlayer = {
+        gender: "0"
+    };
+
+    vm.createPlayer = function(){
+        if(vm.newPlayer.gender === "0"){
+            vm.newPlayer.gender = 0;
+        } else
+            vm.newPlayer.gender = 1;
+        console.log(vm.newPlayer);
+        playersFactory.addPlayer(vm.newPlayer).then(function(response){
+            console.log(response);
+            vm.getPlayers();
+        })
+    }
+
+    vm.deletePlayer = function(playerId){
+        if(confirm('Are you sure you want to delete this player ?')){
+            playersFactory.deletePlayer(playerId).then(function(response){
+                console.log(response);
+                vm.getPlayers();
+            });
+        }
+    }
+
+    vm.getPlayers = function(){
+        playersFactory.getPlayers().then(function(players){
+            vm.players = players;
+            vm.players.forEach(function(player, index){
+                if(player.gender === 0){
+                    player.image = "https://www.w3schools.com/bootstrap/img_avatar3.png";
+                } else
+                    player.image = "https://www.w3schools.com/bootstrap/img_avatar4.png";
+            });
+        });
+    }
+    vm.getPlayers();
 }
 
-PlayersController.$inject = [];
+PlayersController.$inject = ['playersFactory'];
 
 angular.module('characterSheetmanager.playersComponent', []).component('playersComponent', {
     templateUrl: 'players/players.component.html',
@@ -12,7 +52,7 @@ angular.module('characterSheetmanager.playersComponent', []).component('playersC
         parent: '^rootComponent'
     },
     bindings: {}
-}).factory(playersFactory, function($http){
+}).factory('playersFactory', function($http){
     return {
         getPlayers: function(){
             var url = "http://localhost:3000/players";
@@ -21,6 +61,32 @@ angular.module('characterSheetmanager.playersComponent', []).component('playersC
                 url: url
             }).then(function(response){
                 return response.data;
+            }).catch(function(response){
+                console.log("[Error] [getPlayers] . "+response.status+" : "+response.statusText);
+            });
+        },
+        addPlayer: function(player){
+            var url = "http://localhost:3000/players";
+            return $http({
+                method: 'POST',
+                url: url,
+                headers : {
+                    'Content-Type': 'application/json'
+                },
+                data: player
+            }).then(function(response){
+                return response;
+            }).catch(function(response){
+                console.log("[Error] [getPlayers] . "+response.status+" : "+response.statusText);
+            });
+        },
+        deletePlayer: function(playerId){
+            var url = "http://localhost:3000/players/"+playerId;
+            return $http({
+                method: 'DELETE',
+                url: url
+            }).then(function(response){
+                return response;
             }).catch(function(response){
                 console.log("[Error] [getPlayers] . "+response.status+" : "+response.statusText);
             });
